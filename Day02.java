70db259f88fa03cdfebcb2f62fd7ba452a3767fa
prashantkumbhare16 <136996273+prashantkumbhare16@users.noreply.github.com>
2024-07-02 15:27:27 +0530

    day02

---
+++ Day02.java
@@ -0,0 +1,20 @@
+public class Day02 {
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        int b = 20;
+//        float result = (float) a/b;
+//        System.out.println(result);
+
+//        float Marks = 20;
+//        if(Marks >= 80){
+//            System.out.println("A+");
+//        }else if(Marks >= 60){
+//            System.out.println("B+");
+//        }else if(Marks >= 40){
+//            System.out.println("C+");
+//        }else if(Marks < 40){
+//            System.out.println("Fail");
+//        }
+    }
+}
